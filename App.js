5e5e991bcb5ea051c2fc0776f74ff7a35053aa47
turgutguvenc <63226091+turgutguvenc@users.noreply.github.com>
2023-12-06 20:11:25 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -2,26 +2,31 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [output, setOutput] = useState(0);
+  const [converted, setConverted] = useState(0);
   const [amount, setAmount] = useState("");
   const [fromCurrency, setFromCurrency] = useState("EUR");
   const [toCurrency, setToCurrency] = useState("USD");
+  const [isLoading, setIsloading] = useState(false);
 
   useEffect(() => {
     async function getExchangeRate() {
       try {
+        setIsloading(true);
         const res = await fetch(
           `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
         );
         if (!res.ok)
           throw Error("Something went wrong fetching exchange rates");
         const data = await res.json();
-        setOutput(parseFloat(data.rates[toCurrency]).toFixed(2));
+        setConverted(parseFloat(data.rates[toCurrency]).toFixed(2));
+        setIsloading(false);
       } catch (err) {
         console.error(err);
         // Handle errors here, e.g., set an error state or display an error message.
       }
     }
+
+    if (fromCurrency === toCurrency) return setConverted(amount);
 
     if (amount !== "") {
       getExchangeRate();
@@ -54,7 +59,7 @@ export default function App() {
         <option value="INR">INR</option>
       </select>
       <p>
-        {output} {toCurrency}
+        {converted} {toCurrency}
       </p>
     </div>
   );
